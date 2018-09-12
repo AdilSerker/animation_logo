e0b3179700f2a422060ab16e6b96475b93093748
Adil Serkerov <adilserkerov@gmail.com>
2018-09-12 04:42:40 +0300

    logo decompose constructor

--- src/WebGL.ts
+++ src/WebGL.ts
@@ -15,12 +15,9 @@ export class WebGl {
 
     protected clock: Clock;
     protected origin: Vector3;
-
     protected camera: OrthographicCamera;
-
     protected scene: Scene;
     protected light: DirectionalLight;
-
     protected renderer: WebGLRenderer;
 
     protected sceneSubjects: any;
@@ -28,29 +25,14 @@ export class WebGl {
     public constructor(container: HTMLElement) {
         this.canvas = document.createElement('canvas');
         container.appendChild(this.canvas);
-        const { width, height } = this.canvas;
 
         this.clock = new Clock();
         this.origin = new Vector3(0, 0, 0);
 
-        this.scene = new Scene();
-        this.scene.add(new AmbientLight('#F4F4F4'));
-        this.scene.background = new Color('#F4F4F4');
-
-        this.light = new DirectionalLight('#fff', 1);
-        this.light.position.set(-1, -1, -1).normalize();
-        this.scene.add(this.light);
-
-        this.renderer = new WebGLRenderer({ canvas: this.canvas, antialias: true, alpha: true });
-        const DPR = window.devicePixelRatio ? window.devicePixelRatio : 1;
-        this.renderer.setPixelRatio(DPR);
-        this.renderer.setSize(width, height);
-
-        this.renderer.gammaInput = true;
-        this.renderer.gammaOutput = true; 
-
-        this.camera = new OrthographicCamera(width/-2, width/2, height/2, height/-2, 1, 500);
-        this.camera.position.z = 50;
+        this.createScene();
+        this.createLight()
+        this.createRenderer();
+        this.createCamera();
 
         this.bindEventListeners();        
     }
@@ -63,14 +45,42 @@ export class WebGl {
         this.renderer.render(this.scene, this.camera);
     }
 
+    protected createScene() {
+        this.scene = new Scene();
+        this.scene.add(new AmbientLight('#F4F4F4'));
+        this.scene.background = new Color('#F4F4F4');
+    }
+
+    protected createLight() {
+        this.light = new DirectionalLight('#fff', 1);
+        this.light.position.set(-1, -1, -1).normalize();
+        this.scene.add(this.light);
+    }
+
+    protected createRenderer() {
+        const { width, height } = this.canvas;
+        this.renderer = new WebGLRenderer({ canvas: this.canvas, antialias: true, alpha: true });
+        const DPR = window.devicePixelRatio ? window.devicePixelRatio : 1;
+        this.renderer.setPixelRatio(DPR);
+        this.renderer.setSize(width, height);
+
+        this.renderer.gammaInput = true;
+        this.renderer.gammaOutput = true; 
+    }
+
+    protected createCamera() {
+        const { width, height } = this.canvas;
+
+        this.camera = new OrthographicCamera(width/-2, width/2, height/2, height/-2, 1, 500);
+        this.camera.position.z = 50;
+    }
+
     protected bindEventListeners() {
         window.onresize = this.resizeCanvas.bind(this);
-
         this.resizeCanvas();
     }
 
     protected resizeCanvas() {
-               
         this.renderer.setSize(window.innerWidth, window.innerHeight);
         this.camera.updateProjectionMatrix();
     }
